de0d9ce91b666c02bc66fe6d479a1c1f28c9bc60
Yashi Gupta <yashi.12019103@lpu.in>
2023-06-03 04:11:02 +0530

    lms

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,10 @@ import './Footerstyle.css';
 const Footer = () => {
   return (
     <footer className="footer">
+    <h3>Yashi Gupta (12019103) </h3>
+    <p>yashig52@gmail.com</p>
       <p>&copy; 2023 Your Company. All rights reserved.</p>
+      
     </footer>
   );
 };
